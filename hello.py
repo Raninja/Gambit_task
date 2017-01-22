f439d3cb742d698a8d066a85ae4b6987da2b77ea
Raninja <ranjita.pradhan@gmail.com>
2017-01-22 23:00:03 +0200

    Removed unnecessary print commands

--- hello.py
+++ hello.py
@@ -10,26 +10,8 @@ def main():
     #Read the registers and put them into a dict
     lines = open('input.txt','rt').readlines()
     register_dict = {int(l.split(':')[0]): int(l.split(':')[1]) for l in lines[1:]}
-    #print('lines')
-    #print(lines)
-    #print()
-    #print('register_dict')
-    #print()
-    #print(register_dict)
-    #Let's output some
-
-    #From the manual:
-    #Long is a signed 4-byte integer
-    #Register 21-22, Negative energy accumulator is -56.
-
-    #FLOAT / REAL4 singular ieee-754 number
-    #Register 33-34, Temperature #1/inlet is 7.101173400878906.
-
-    #From the manual: high byte is step, low byte is signal quality
-    #I guess low byte is the last 4 bits of the input
-
-    #Register 92, Signal Quality is 38.
-
+   
+   
     input_list = [(1,2),(3,4),(5,6),(7,8),(9,10),(11,12),(13,14),(15,16),(17,18),(19,20),(21,22),(23,24),(25,26),(27,28),(29,30),(31,32),(33,34),(35,36),(37,38),(39,40),(41,42),(43,44),(45,46),(47,48),(49,50),(51,),(53,55),(56,),(59,),(60,),(61,),(62,),(72,),(77,78),(79,80),(81,82),(83,84),(85,86),(87,88),(89,90),(92,),(93,),(94,),(96,),(97,98),(99,100)]
 
     #register_ops = {}
